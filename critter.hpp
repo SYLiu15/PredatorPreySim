90478fb83869b4d5f16615b1b0ec5c2ea5d8ef90
Angela Wen <wena@oregonstate.edu>
2018-07-25 17:39:22 +0800

    include virtual destructor

--- critter.hpp
+++ critter.hpp
@@ -50,7 +50,7 @@ public:
 	//virtual void breed() = 0;
 	//virtual void starve() = 0;
 	
-	//virtual ~Critter() = default; // Default virtual constructor
+	virtual ~Critter() = default; // Default virtual destructor
 
 };
 
