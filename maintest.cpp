2095a2401244c5f28f46aa8fe5052ad552365acf
SYLiu15 <35639224+SYLiu15@users.noreply.github.com>
2018-07-27 01:31:45 -0700

    Main update
    Added prompts for board size, number of ants, and number of doodlebugs. Added UI indicators on number ranges, implemented error handling.
    Added function to randomize starting location of all bugs.
    Changed stepsSinceBreeding increment location.

--- maintest.cpp
+++ maintest.cpp
@@ -11,14 +11,36 @@ using std::string;
 #include "critter.hpp"
 #include "doodlebug.hpp"
 
-void print_board(Critter ***array, int arrayLength, int arrayWidth);
-void reset_flags(Critter ***array, int arrayLength, int arrayWidth);
+void place_bugs(Critter ***, int, int);
+void place_ants(Critter ***, int, int);
+void place_doodlebugs(Critter ***, int, int);
+
+void print_board(Critter ***, int, int);
+void reset_flags(Critter ***, int, int);
+int getChoice(int, int);
 
 
 int main() {
-	srand(time(NULL));
+	cout << "\n\n\n\n";
+	cout << "***********************************************" << endl;
+	cout << "Welcome to Group 13's Predator-Prey Simulation." << endl;
+	cout << "***********************************************" << endl;
+	// Set a random generator seed
+	// Referenced from Program 3-32 of C++ Early Objects, 9th Edition
+	unsigned seed;
+
+	seed = static_cast<unsigned>(time(0));
+	srand(seed);
+
+	cout << "\nThe simulation will occur on a grid with user provided dimensions" << endl;
+	cout << "The number of ants and doodlebugs will be provided by the user and the placement randomized" << endl;
+	cout << "\nPlease enter the number of rows for the grid (min-2, max-50): ";
+	int arrayLength = getChoice(2, 50);
+
+	cout << "\nPlease enter the number of columns for the grid (min-2, max-50): ";
+	int arrayWidth = getChoice(2, 50);
 	
-	int arrayLength = 5, arrayWidth = 5;
+	//int arrayLength = 5, arrayWidth = 5;
 	
 	//initialize array of Critter pointers
 	Critter ***array = new Critter**[arrayLength];
@@ -29,15 +51,8 @@ int main() {
 		}
 	}
 	
-	//create single ant
-	array[2][2] = new Ant(2,2,0,false);
-	array[2][3] = new Ant(2,3,0,false);
-	array[1][2] = new Ant(1,2,0,false);
-	array[0][2] = new Ant(0,2,0,false);
-	array[2][0] = new Ant(2,0,0,false);
-
-	//create new doodlebug
-	array[4][4] = new Doodlebug(4,4,0,false);
+	//prompt for and randomize bug location
+	place_bugs(array, arrayLength, arrayWidth);
 	
 	//run simulation, repeat until user types 'n'
 	int value = 1;
@@ -93,9 +108,64 @@ int main() {
 	
 }
 
+
+void place_bugs(Critter ***array, int arrayLength, int arrayWidth) {
+	//get number of ants
+	cout << "\nEnter the number of ants (min-1, max-" << arrayLength*arrayWidth-1 << "): ";
+	int ants = getChoice(1, arrayLength*arrayWidth-1);
+	
+	//get number of doodlebugs
+	if (ants < arrayLength*arrayWidth-1) {
+		cout << "\nEnter the number of doodlebugs (min-1, max-" << arrayLength*arrayWidth - ants << "): ";
+	}
+	else {
+		cout << "\nEnter the number of doodlebugs (max-1): ";
+	}
+	int doodle = getChoice(1, arrayLength*arrayWidth - ants);
+	
+	//initialize temp array
+	int **intArray = new int*[arrayLength];
+	for (int i = 0; i < arrayLength; i++) {
+		intArray[i] = new int[arrayWidth];
+		for (int j = 0; j < arrayWidth; j++) {
+			intArray[i][j] = 0;
+		}
+	}
+	
+	while (ants > 0) {
+		place_ants(array, arrayLength, arrayWidth);
+		ants--;
+	}
+	while (doodle > 0) {
+		place_doodlebugs(array, arrayLength, arrayWidth);
+		doodle--;
+	}
+}
+
+void place_ants(Critter ***array, int arrayLength, int arrayWidth) {
+	int row, col;
+	do {
+		row = rand()%arrayLength;
+		col = rand()%arrayWidth;
+	} while (array[row][col] != nullptr);
+	
+	array[row][col] = new Ant(row,col,0,false);
+}
+
+void place_doodlebugs(Critter ***array, int arrayLength, int arrayWidth) {
+	int row, col;
+	do {
+		row = rand()%arrayLength;
+		col = rand()%arrayWidth;
+	} while (array[row][col] != nullptr);
+	
+	array[row][col] = new Doodlebug(row,col,0,false);
+}
+
+
 void print_board(Critter ***array, int arrayLength, int arrayWidth) {
 	cout << endl;
-	for (int i = 0; i < arrayLength+2; i++) {
+	for (int i = 0; i < arrayWidth+2; i++) {
 		cout << "-";
 	}
 	cout << endl;
@@ -111,7 +181,7 @@ void print_board(Critter ***array, int arrayLength, int arrayWidth) {
 		}
 		cout << "|" << endl;
 	}
-	for (int i = 0; i < arrayLength+2; i++) {
+	for (int i = 0; i < arrayWidth+2; i++) {
 		cout << "-";
 	}
 	cout << endl;
@@ -125,4 +195,30 @@ void reset_flags(Critter ***array, int arrayLength, int arrayWidth) {
 			}
 		}
 	}
+}
+
+int getChoice(int minChoice, int maxChoice) {
+	int input;
+	//cout << "Please enter your choice: ";
+	cin >> input;
+
+	// Try again if input is out of range or fails
+	while (input < minChoice || input > maxChoice || cin.fail()) {
+		if (maxChoice == minChoice) {
+			cout << "Input must be " << maxChoice << ", ";
+		}
+		else {
+			cout << "Input must be an integer between " << minChoice << " and " << maxChoice << ", ";
+		}
+		cout << "please retry input: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> input;
+	}
+
+	// Flush buffer after successful input
+	cin.clear();
+	cin.ignore(256, '\n');
+
+	return input;
 }
